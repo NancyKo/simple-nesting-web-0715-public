b62b4edddb4a5e7faf6119a55959d9377501ee80
NancyKo <konancys@gmail.com>
2015-07-31 07:45:31 -0400

    Not finished

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -55,6 +55,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  language = programmer_hash[:dennis_ritchies][:languages]
+  language.to_a
 end
 
 def adding_matz
@@ -80,7 +82,7 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => "LISP, C"}
 end
 
 def changing_alan
@@ -101,6 +103,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+
      
      
 end
@@ -121,8 +125,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
-     
+     programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"   
 end
 
 
